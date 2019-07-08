229a27b02ee62a0575322d647573f2c5ff3175e0
Siddhant <siddhant.gupta@dailyobjects.com>
2019-07-08 17:57:38 +0530

    code refactor remove failing condition

--- tests/googlebot.py
+++ tests/googlebot.py
@@ -7,18 +7,14 @@ options.add_argument(
 driver = webdriver.Chrome(options=options)
 driver.get("https://www.dailyobjects.com/")
 html_source = driver.page_source
-print(html_source)
 
-if ('header-row' in html_source):
+if 'header-row' in html_source:
     print('Header row is present')
-if ('do-logo' in html_source):
+if 'do-logo' in html_source:
     print('DailyObjects logo is present')
-if ('smartphone page' in html_source):
+if 'smartphone page' in html_source:
     print('Smartphone navigation item is present')
-if ('Bags And Sleeves Page' in html_source):
+if 'Bags And Sleeves Page' in html_source:
     print('Bags and sleeves nav item is present')
-if (
-        'https://ik.imagekit.io/dailyobjects/Offers/22-06-19/extra-10-off-desktop.jpg?tr=bg-FFFFFF,pr-true,q-100' in html_source):
-    print('Offer banner is present')
-if ('bannerImage' in html_source):
+if 'bannerImage' in html_source:
     print('Carousel banner is present')
