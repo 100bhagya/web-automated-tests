8a8b1c68c98a68855276fca6fb3cc958c67d508e
Saubhagya <saubhagya@dailyobjects.com>
2019-07-11 17:42:16 +0530

    * listing and product page added * unittest implementation

--- tests/googlebot.py
+++ tests/googlebot.py
@@ -1,36 +1,55 @@
+import requests
+import unittest
 import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options, DesiredCapabilities
 
-switch = {
-    "live" : "www",
-    "stage" : "stage",
-    "dev" : "dev"
-}
 
-api = switch.get(sys.argv[1])
+class testing_googelbot(cls):
+    driver = webdriver.Remote
+    url = ""
+    @classmethod
+    def setUpClass(cls):
+        useragent = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+        options = Options()
+        options.add_argument('user-agent= ' + useragent)
+        cls.driver = webdriver.Remote(
+            command_executor='http://0.0.0.0:4444/wd/hub',
+            desired_capabilities=DesiredCapabilities.CHROME
+        )
+        cls.switch_url = {
+            "live" : "www",
+            "stage" : "stage",
+            "dev" : "dev"
+        }
+        cls.api = cls.switch.get(sys.argv[1])
 
-url = "https://" + api + ".dailyobjects.com/"
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
 
-options = Options()
-options.add_argument(
-    'user-agent= Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)')
+    def test_home_page(self):
+        url = "https://" + self.api + ".dailyobjects.com/"
+        self.driver.get(url)
+        self.assertIn('header-row', self.driver.page_source)
+        self.assertIn('do-logo', self.driver.page_source)
+        self.assertIn('Hot Selling', self.driver.page_source)
 
-driver = webdriver.Remote(
-    command_executor='http://0.0.0.0:4444/wd/hub',
-    desired_capabilities=DesiredCapabilities.CHROME
-)
+    def test_product_page(self):
+        url = "https://" + self.api + ".dailyobjects.com/dailyobjects-teal-blue-glass-case-cover-for-iphone-x"
+        self.driver.get(url)
+        self.assertIn('ngx-json-ld', self.driver.page_source)
 
-driver.get(url)
-html_source = driver.page_source
+    def test_listing_page(self):
+        url = "https://" + self.api + ".dailyobjects.com/designer-cases/apple/iphone-x/glass"
+        self.driver.get(url)
+        response = requests.get(url)
+        if response.ok:
+            print(response.status_code)
+        else:
+            print('Bad Response')
 
-if 'header-row' in html_source:
-    print('Header row is present')
-if 'do-logo' in html_source:
-    print('DailyObjects logo is present')
-if 'smartphone page' in html_source:
-    print('Smartphone navigation item is present')
-if 'Bags And Sleeves Page' in html_source:
-    print('Bags and sleeves nav item is present')
-if 'bannerImage' in html_source:
-    print('Carousel banner is present')
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    unittest.main(argv=[url])
